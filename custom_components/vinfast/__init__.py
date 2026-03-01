df4d4538543adce619369f2607957b22711f6528
thangnd85 <thangnd85@gmail.com>
2026-03-01 16:09:45 +0700

    Implement MQTT stream cleanup on unload
    Added cleanup for old MQTT stream during entry unload.

--- custom_components/vinfast/__init__.py
+++ custom_components/vinfast/__init__.py
@@ -25,10 +25,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = {"api": api}
 
-    # Kích hoạt luồng MQTT chạy ngầm 1 lần duy nhất tại đây
     await hass.async_add_executor_job(api.start_mqtt)
 
-    # Nạp cả 2 nền tảng: Cảm biến và Bản đồ
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "device_tracker"])
 
     return True
@@ -36,5 +34,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor", "device_tracker"])
     if unload_ok:
+        api = hass.data[DOMAIN][entry.entry_id]["api"]
+        # --- ĐIỂM FIX LỖI TẢI LẠI: Dọn dẹp sạch luồng cũ ---
+        await hass.async_add_executor_job(api.stop)
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
