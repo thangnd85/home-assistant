2580df643d61ca1e882bd414a47e79611a1cdcbb
thangnd85 <thangnd85@gmail.com>
2026-03-03 21:15:24 +0700

    Delete custom_components/lich_am directory

--- custom_components/lich_am/const.py
+++
@@ -1,7 +0,0 @@
-"""Constants for the Lich Am integration."""
-from datetime import timedelta
-
-DOMAIN = "lich_am"
-CONF_NAME = "name"
-DEFAULT_NAME = "Lịch Âm"
-SCAN_INTERVAL = timedelta(minutes=60) # Cập nhật mỗi 10 phút
